4ec118f548cf9e276085732501fa0915adc70c69
Paul Molins <pro@paul-molins.fr>
2025-10-25 12:04:53 -0400

    new version

---
+++ assets/NavbarShortcuts-yj-uwso1.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NavbarShortcuts.vue_vue_type_script_setup_true_lang-DpZRSc8a.js";import"./index-BzB504rc.js";export{m as default};
---
+++ assets/constants-CCDRJ4Ay.js
@@ -0,0 +1,1 @@
+const _=31536e3;export{_ as C};
